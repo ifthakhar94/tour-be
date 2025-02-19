b02d8f05618cb80dbc99d7d687bae7d7b2dd1306
Iftekhar Mahmud <ifthakhar8@gmail.com>
2025-02-19 09:56:29 +0600

    Add sorting on api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,12 +2,35 @@ const Tour = require("../models/tourModel");
 
 exports.getAllTours = async (req, res) => {
   try {
+    // Build query
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((el) => delete queryObj[el]);
-    const query = await Tour.find(queryObj);
-    const tours = query.slice(0, 10);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr)); // Remove await here
+
+    // Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    }
+
+    // Execute query
+    const tours = await query; // Add await here
+
+    // Send response
     res.status(200).json({
       status: "success",
       results: tours.length,
